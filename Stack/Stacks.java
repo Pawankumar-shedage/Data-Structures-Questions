5609fd8ffbb45420b6e94279ed46ae7a9045dbe0
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-08-16 13:13:59 +0530

    Reverse of Stack Done✅

--- Stack/Stacks.java
+++ Stack/Stacks.java
@@ -7,19 +7,19 @@ public class Stacks {
 //	Gate Smasher Video on recursion!!.(for revision).
 	public static void pushAtBottom(int data,  Stack<Integer> s)
 	{
-		System.out.println("Im here"+ s);
+//		System.out.println("Im here"+ s);
 		
 		//Base case new data.
 		if(s.isEmpty())
 		{
 			s.push(data);
-			System.out.println(data);
+			
 			return;
 		}
 		// removing top element.
 		int top = s.pop();
 		
-		System.out.println("Poping remaining "+ top);
+//		System.out.println("Poping remaining "+ top);
 		//recursion, till stack is not empty(isEmpty) remove elements.
 		
 		pushAtBottom(data,s); //when base case is reached the function is executed beyond this point, pushing elements from top to the stack.
@@ -27,11 +27,28 @@ public class Stacks {
 //		System.out.println("after recur"+ top+" "+ s);
 		s.push(top);
 		
-//		System.out.println(s);
+	}
+	
+	public static void reverseStack(Stack<Integer> s)
+	{
+		//base case;
+		if(s.isEmpty())
+		{
+//			System.out.println("Hi");
+			return;
+		}
 		
+		int top = s.pop();
+		reverseStack(s);
+		
+		//below code will execute after the base condition is true.
+		
+		pushAtBottom(top, s);
 		
 		
 	}
+
+	
 
 	public static void main(String[] args) {
 		
@@ -43,15 +60,18 @@ public class Stacks {
 //		System.out.println(s.search(5)); //1 based position from top of the stack.
 		
 		//push at bottom of the stack.
-		pushAtBottom(1,s);   
+//		pushAtBottom(1,s);   
 		
+		s.push(6);
 		
+		reverseStack(s);
 		
 		while(!s.isEmpty())
 		{
-			System.out.println(s.peek());
+			System.out.println(s.peek() + " ");
 			s.pop();
 		}
+		
 		
 	}
 }
